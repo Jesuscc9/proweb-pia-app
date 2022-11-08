b14ae6422ec9e1a8d309122b07134cbf0d550631
Jesus Cervantes <jesusuper9@gmail.com>
2022-11-08 12:46:56 -0600

    create form controller with posts

--- src/js/controllers/form_controller.js
+++ src/js/controllers/form_controller.js
@@ -10,6 +10,8 @@ export default class extends Controller {
   }
 
   connect() {
+    fetchPosts()
+
     this.element.addEventListener('submit', async (evt) => {
       evt.preventDefault()
 
@@ -25,7 +27,8 @@ export default class extends Controller {
 
       try {
         await axios.post(URL, values)
-        // this.opts.onSuccess()
+        this.element.reset()
+        fetchPosts()
       } catch (e) {
         console.error({ e })
 
@@ -35,6 +38,34 @@ export default class extends Controller {
       } finally {
         this.buttonTarget.textContent = prevButtonText
       }
+    })
+  }
+}
+
+const fetchPosts = async () => {
+  const $posts = document.querySelector('#posts-container')
+  const res = await axios.get(`${BASE_URL}/posts/index.php`)
+
+  const posts = res.data.data
+
+  console.log({ res })
+
+  $posts.innerHTML = 'No posts found'
+
+  if (posts?.length > 0) {
+    $posts.innerHTML = ''
+    posts.forEach((post) => {
+      console.log({ post })
+      console.log(new Date(post.created_at))
+
+      $posts.innerHTML =
+        $posts.innerHTML +
+        `
+        <div>
+          <p class='text-red-500'>${post.body}</p>
+          <p class='text-white'>${post.created_at}</p>
+        </div>
+      `
     })
   }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import { Application } from '@hotwired/stimulus'
-
 import FormController from './controllers/form_controller'
+
+import '../css/style.css'
 
 window.Stimulus = Application.start()
 window.Stimulus.register('form', FormController)
