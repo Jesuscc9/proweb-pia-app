578112e308f24dc29b7a8684fc592be40fd1d590
Jesus Cervantes <jesusuper9@gmail.com>
2022-11-26 01:29:37 -0600

    add more design, profile picture

--- src/js/controllers/posts_controller.js
+++ src/js/controllers/posts_controller.js
@@ -2,6 +2,14 @@ import { Controller } from '@hotwired/stimulus'
 import { BASE_URL } from '../config'
 import axios from 'axios'
 
+import TimeAgo from 'javascript-time-ago'
+
+import es from 'javascript-time-ago/locale/es'
+
+TimeAgo.addDefaultLocale(es)
+
+const timeAgo = new TimeAgo('es')
+
 export default class extends Controller {
   static targets = ['container', 'refreshButton']
 
@@ -9,7 +17,6 @@ export default class extends Controller {
     const $posts = this.containerTarget
     this.refreshButtonTarget.disabled = true
 
-    await new Promise((resolve, reject) => setTimeout(resolve, 300))
     const res = await axios.get(`${BASE_URL}/posts/index.php`)
 
     const posts = res.data.data
@@ -26,21 +33,43 @@ export default class extends Controller {
         $posts.innerHTML =
           $posts.innerHTML +
           `
-          <div class='border p-4 w-full'>
+          <div class='border p-4 w-full flex flex-col gap-2'>
             ${
               showActions
-                ? '<button class="bg-red-500 rounded-md py-2 px-4" data-controller-id>hola</button>'
+                ? '<button class="bg-red-500 rounded-md py-2 px-4" data-controller-id>Delete</button>'
                 : ''
             }
+            <div class='flex gap-2 items-center'>
+              <img src='${
+                post.author.avatar_url
+              }' class='w-8 h-8 rounded-full' alt="" />
+
+              <div class='flex gap-2'>
+                <p class='font-bold mt-0'>${post.author.username}</p>
+                <span class='opacity-60'>·</span>
+                <p class='text-white opacity-60'>${timeAgo.format(
+                  new Date(post.created_at)
+                )}</p>
+              </div>
+            </div>
+
             <p class='text-red-500'>${post.body}</p>
-            <p class='text-white'>${post.created_at}</p>
-          </div>
+
+            <div class='mt-4 flex justify-around'>
+              <button class='bg-red-500 px-2'>like</button>
+              <button class='bg-red-500 px-2'>comment</button>
+              
+            </div>
+
+         </div>
         `
 
         $posts.querySelectorAll('div').forEach((div, i) => {
           const eraseButton = div.querySelector('button')
+          console.log({ $posts })
 
           eraseButton?.addEventListener('click', async () => {
+            console.log({ posts, i })
             await axios.delete(`${BASE_URL}/posts/index.php?id=${posts[i].id}`)
             this.refresh()
           })
