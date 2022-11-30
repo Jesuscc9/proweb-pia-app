4674ac78f651fdffb34d56b9dfdc277099a5837d
Jesus Cervantes <jesusuper9@gmail.com>
2022-11-29 18:34:24 -0600

    ready

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_URL
+export const BASE_URL = 'https://dissoftec.com'
 
 export const LOADER = `
 <svg
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,4 @@
 export const redirect = (page) => {
-  const newUrl = window.location.origin + '/src/pages/' + page + '/index.html'
+  const newUrl = window.location.origin + '/pages/' + page + '/'
   window.location.href = newUrl
 }
